cc3523a4254054e97fde7043e1bcf89d77283cdb
Nikita Garmash <nikita.shlapunov71@gmail.com>
2023-08-04 16:44:41 +0300

    test commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,32 @@
-  
+const inputElement = document.getElementById('phone-number');
+const requiredElement = document.querySelector('.star-phone');
+
+inputElement.addEventListener('input', function() {
+  if (inputElement.value !== '') {
+    requiredElement.style.display = 'none';
+  } else {
+    requiredElement.style.display = 'block';
+  }
+});
+
+const mailElement = document.getElementById('user-email');
+const starElement = document.querySelector('.star-mail');
+
+mailElement.addEventListener('input', function() {
+  if (mailElement.value !== '') {
+    starElement.style.display = 'none';
+  } else {
+    starElement.style.display = 'block';
+  }
+});
+
+const messageElement = document.getElementById('message');
+const messageStarElement = document.querySelector('.star-message');
+
+messageElement.addEventListener('input', function() {
+  if (messageElement.value !== '') {
+    messageStarElement.style.display = 'none';
+  } else {
+    messageStarElement.style.display = 'block';
+  }
+});
